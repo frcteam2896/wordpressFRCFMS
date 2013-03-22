2afa128553610879099f580a162373e660c455ff
MechaMonarchs <team2896damien@gmail.com>
2013-03-21 18:31:25 -1000

    Basic metadata written (Still non-functional)

---
+++ frcMain.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: FRC Blue Alliance
+Plugin URI: https://github.com/MechaMonarchs/wordpressBlueAlliance
+Version: 0.0.1
+Author: Damien MechaMonarchs (FRC Team 2896)
+Description: Integrates feeds from The Blue Alliance, a statistics site for FIRST Robotics
+*/
+
+?>
