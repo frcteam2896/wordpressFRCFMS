a396e7fb716de370e7e5c259fa3f27f9c52fe7b5
MechaMonarchs <team2896damien@gmail.com>
2013-03-24 12:13:07 -1000

    Retry framework… :(

--- frcMain.php
+++ frcMain.php
@@ -6,80 +6,45 @@ Version: 0.0.3
 Author: Damien MechaMonarchs (FRC Team 2896)
 Description: Integrates feeds from The Blue Alliance, a statistics site for FIRST Robotics
 */
-class TBA_Widget extends WP_Widget {
+class RandomPostWidget extends WP_Widget
+{
+  function RandomPostWidget()
+  {
+    $widget_ops = array('classname' => 'RandomPostWidget', 'description' => 'Displays a random post with thumbnail' );
+    $this->WP_Widget('RandomPostWidget', 'Random Post and Thumbnail', $widget_ops);
+  }
 
-	public function __construct() {
-		// widget actual processes
-		$widget_ops = array( 'classname' => 'TBA_Widget', 'description' => 'The Blue Alliance integration' );
-		$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'TBA_wp' );
-		parent::__construct(
-		    'tba_widget', //Base ID
-		    'TBA_Widget', //Name
-		    $widget_ops,
-		    $control_ops,
-		    );
+  function form($instance)
+  {
+    $instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
+    $title = $instance['title'];
+?>
+  <p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
+<?php
+  }
 
-	}
-    /**
-	 * Front-end display of widget.
-	 *
-	 * @see WP_Widget::widget()
-	 *
-	 * @param array $args     Widget arguments.
-	 * @param array $instance Saved values from database.
-	 */
-	public function widget( $args, $instance ) {
-		// outputs the content of the widget
-		extract($args);
-		$title = apply_filters( 'widget_title', $instance['title']);
+  function update($new_instance, $old_instance)
+  {
+    $instance = $old_instance;
+    $instance['title'] = $new_instance['title'];
+    return $instance;
+  }
 
-		echo $before_widget;
-		if (!empty($title))
-		echo $before_title.$title.$after_title;
-		echo __('Hello, World! <br/> Team #'.$team, 'text_domain');
-		echo $after_widget;
-	}
-    /**
-	 * Sanitize widget form values as they are saved.
-	 *
-	 * @see WP_Widget::update()
-	 *
-	 * @param array $new_instance Values just sent to be saved.
-	 * @param array $old_instance Previously saved values from database.
-	 *
-	 * @return array Updated safe values to be saved.
-	 */
+  function widget($args, $instance)
+  {
+    extract($args, EXTR_SKIP);
 
-    public function update( $new_instance, $old_instance ) {
-    // processes widget options to be saved
-        $instance = $old_instance;
+    echo $before_widget;
+    $title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
 
-        $instance['title'] = strip_tags($new_instance['title']);
-        $instance['team'] = strip_tags($new_instance['team']);
+    if (!empty($title))
+      echo $before_title . $title . $after_title;;
 
-        return $instance;
-    }
+    // WIDGET CODE GOES HERE
+    echo "<h1>This is my new widget!</h1>";
 
-	/**
-	 * Back-end widget form.
-	 *
-	 * @see WP_Widget::form()
-	 *
-	 * @param array $instance Previously saved values from database.
-	 */
- 	public function form( $instance ) {
-		/* Set up some default widget settings. */
-		$defaults = array( 'title' => 'Current Matches', 'team' => '2896');
-		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
-
-<label for="<?php echo $this->get_field_id( 'title' );?>"><?php _e( 'Title:' ); ?></label>
-<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
-<label for="<?php echo $this->get_field_id('team'); ?>"><?php _e( 'Team #:' ); ?></label>
-<input class="widefat" id="<?php echo $this->get_field_id( 'team' ); ?>" name="<?php echo $this->get_field_name('team'); ?>" type="text" value="<?php echo esc_attr($team); ?>" />
-</p>
-    <?
-	}
-
+    echo $after_widget;
+  }
 
 }
-add_action( 'widgets_init', create_function( '', 'register_widget( "TBA_Widget" );' ) );
+add_action( 'widgets_init', create_function('', 'return register_widget("RandomPostWidget");') );?>
