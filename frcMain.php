c71ffea054367a233c9fcb825e62e564be8d6e02
MechaMonarchs <team2896damien@gmail.com>
2013-03-25 19:43:01 -1000

    Parsing function written

--- frcMain.php
+++ frcMain.php
@@ -1,17 +1,36 @@
 <?php
 /*
 Plugin Name: FRC Blue Alliance
-Plugin URI: https://github.com/MechaMonarchs/wordpressBlueAlliance
+Plugin URI: https://github.com/MechaMonarchs/wordpressFRCFMS
 Version: 0.0.3
 Author: Damien MechaMonarchs (FRC Team 2896)
-Description: Integrates feeds from The Blue Alliance, a statistics site for FIRST Robotics
+Description: Integrates data from FRCFMS, a score reporting site for FIRST Robotics
 */
-class TBA_Widget extends WP_Widget
+class FMS_Widget extends WP_Widget
 {
-  function TBA_Widget()
+  function TBA_Parser($team, $event)
+   {
+        $data = file_get_contents('https://search.twitter.com/search.json?q=%23'.$event.'%20from:frcfms');
+        $data = json_decode($data);
+        $results = count($data -> results);
+        for($i = 0, $i <= $results, $i++) {
+            $frcfms = $data -> results[$i] -> text;
+            $frcfms = preg_split("(#FRC\w* |TY | MC | RF | BF | RA | BA | RC | BC | RFP | BFP | RAS | BAS | RTS | BTS )", $frcfms);
+            if (in_array($team))
+                $i = $results + 1;
+        }
+        return array(
+            "match" => $frcfms[3],
+            "red" => $frcfms[4],
+            "blue" => $frcfms[5],
+            "rAlliance" => explode(" ",$frcfms[6]),
+            "bAlliance" => explode(" ",$frcfms[7]),
+            )
+   }
+  function FMS_Widget()
   {
-    $widget_ops = array('classname' => 'TBA_Widget', 'description' => 'Displays a random post with thumbnail' );
-    $this->WP_Widget('TBA_Widget', 'The Blue Alliance integration', $widget_ops);
+    $widget_ops = array('classname' => 'FMS_Widget', 'description' => 'Integrates match feeds from The Blue Alliance' );
+    $this->WP_Widget('FMS_Widget', 'FMS Feed', $widget_ops);
   }
 
   function form($instance)
@@ -20,6 +39,7 @@ class TBA_Widget extends WP_Widget
     $title = $instance['title'];
 ?>
   <p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
+  <p><label for="<?php echo $this->get_field_id('team'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('team'); ?>" name="<?php echo $this->get_field_name('team'); ?>" type="text" value="<?php echo attribute_escape($team); ?>" /></label></p>
 <?php
   }
 
@@ -27,6 +47,7 @@ class TBA_Widget extends WP_Widget
   {
     $instance = $old_instance;
     $instance['title'] = $new_instance['title'];
+    $instance['team'] = $new_instance['team'];
     return $instance;
   }
 
@@ -47,4 +68,4 @@ class TBA_Widget extends WP_Widget
   }
 
 }
-add_action( 'widgets_init', create_function('', 'return register_widget("TBA_Widget");') );?>
+add_action( 'widgets_init', create_function('', 'return register_widget("FMS_Widget");') );?>
