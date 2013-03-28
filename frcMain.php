37fa1a5bea8af15c194f3d984fef7fc12617173d
MechaMonarchs <team2896damien@gmail.com>
2013-03-27 18:08:22 -1000

    v1.1b

--- frcMain.php
+++ frcMain.php
@@ -2,19 +2,21 @@
 /*
 Plugin Name: FRCFMS widget
 Plugin URI: https://github.com/MechaMonarchs/wordpressFRCFMS
-Version: 0.1.0
+Version: 0.1.1(b)
 Author: Damien MechaMonarchs (FRC Team 2896)
-Description: Integrates data from FRCFMS, a score reporting site for FIRST Robotics
+Description: Integrates data from FRCFMS, a score reporting site for FIRST Robotics.  Includes shortcode in format [FMS team=#### event=FRC(event code) data=(type, match, red, blue, rAlliance, bAlliance')] where "type" is the match type (practice, qualifier, or elimination), "match" is the match number, "red" is the red alliance's score, "blue" is the blue alliance's score, "rAlliance" is the teams in the Red Alliance, and "bAlliance" is the teams in the Blue Alliance.
 */
 class FMS_Widget extends WP_Widget
 {
   function TBA_Parser($team, $event){
         $data = file_get_contents('https://search.twitter.com/search.json?q=%23'.$event.'%20from:frcfms');
+        if (empty($data->results))
+            return(NULL);
         $data = json_decode($data);
         $n = count($data -> results);
         for($i = 0; $i < $n; $i++){
             $frcfms = $data -> results[$i] -> text;
-            if (preg_match('/'.$frcfms.'/',$team))
+            if (preg_match('/'.$team.'/',$frcfms))
                 $i = $n + 1;
         }
         $frcfms = preg_split("(#FRC\w* |TY | MC | RF | BF | RA | BA | RC | BC | RFP | BFP | RAS | BAS | RTS | BTS )", $frcfms);
@@ -74,17 +76,20 @@ class FMS_Widget extends WP_Widget
 
     // WIDGET CODE GOES HERE
     $match = $this->TBA_Parser($instance['team'], $instance['event']);
-    echo($match["match"].$match["type"].'<br/>');
-    echo('<div class="fmsred"><br/>Red:<br/>'.$match["rAlliance"][0].'<br/>');
-    echo($match["rAlliance"][1].'<br/>');
-    echo($match["rAlliance"][2].'<br/>');
-    echo('<div class="sred">Score:'.$match["red"].'<br/></div><div>');
-    echo('<div class="fmsblue"><br/>Blue:</br>'.$match["bAlliance"][0].'<br/>');
-    echo($match["bAlliance"][1].'<br/>');
-    echo($match["bAlliance"][2].'<br/>');
-    echo('<div class="sblue">Score:'.$match["Blue"].'<br/></div></div>');
-    echo $after_widget;
+    if($match==NULL)
+        echo("Could not find match data.  Has the event started yet?");
+    else{
+        echo($match["match"].$match["type"].'<br/>');
+        echo('<div class="fmsred"><br/>Red:<br/>'.$match["rAlliance"][0].'<br/>');
+        echo($match["rAlliance"][1].'<br/>');
+        echo($match["rAlliance"][2].'<br/>');
+        echo('<div class="sred">Score:'.$match["red"].'<br/></div><div>');
+        echo('<div class="fmsblue"><br/>Blue:</br>'.$match["bAlliance"][0].'<br/>');
+        echo($match["bAlliance"][1].'<br/>');
+        echo($match["bAlliance"][2].'<br/>');
+        echo('<div class="sblue">Score:'.$match["Blue"].'<br/></div></div>');
+        echo $after_widget;
+    }
   }
-
 }
 add_action( 'widgets_init', create_function('', 'return register_widget("FMS_Widget");') );?>
